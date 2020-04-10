6bf0c1bf89bb7ce7d12197daef9095795fa93738
Harry Coa <rodol_2000@hotmail.com>
2020-04-10 09:09:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# microserviciosSB
+ejemplo de microservicios desarrollado con java spring boot
---
+++ src/main/java/com/formaciondbi/microservicios/app/usuarios/services/AlumnoService.java
@@ -0,0 +1,17 @@
+package com.formaciondbi.microservicios.app.usuarios.services;
+
+import java.util.Optional;
+
+import com.formaciondbi.microservicios.app.usuarios.models.entity.Alumno;
+
+public interface AlumnoService {
+
+    public Iterable<Alumno> findAll();
+
+    public Optional<Alumno> findById(Long id);
+
+    public Alumno save(Alumno alumno);
+
+    public void deleteById(Long id);
+
+}
